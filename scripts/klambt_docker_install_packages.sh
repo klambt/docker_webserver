7eb4c37fc36b49fa438f82295b9a5609b6a7bd7e
Weyand Tim <tim.weyand@R1717.local>
2016-08-12 12:50:26 +0200

    Clean Up
    At the end of the build we should clean up

--- scripts/klambt_docker_install_packages.sh
+++ scripts/klambt_docker_install_packages.sh
@@ -7,7 +7,6 @@ echo '#############################################'
 echo ' '
 
 apt-get install -y $(grep -vE "^\s*#" /root/conf/apt-get_packages.conf  | tr "\n" " ")
-rm -rf /var/lib/apt/lists/* 
 docker-php-ext-configure gd --with-png-dir=/usr --with-jpeg-dir=/usr
 docker-php-ext-install $(grep -vE "^\s*#" /root/conf/php_extentions.conf  | tr "\n" " ")
 a2enmod rewrite
