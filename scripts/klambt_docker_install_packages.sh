41a4f833a429da0bb75c8daae891e6d3f4ec55d4
Weyand Tim <tim.weyand@R1717.local>
2016-08-25 09:49:13 +0200

    Enable Socket for PHP

--- scripts/klambt_docker_install_packages.sh
+++ scripts/klambt_docker_install_packages.sh
@@ -9,4 +9,5 @@ echo ' '
 apt-get install -y $(grep -vE "^\s*#" /root/conf/apt-get_packages.conf  | tr "\n" " ")
 docker-php-ext-configure gd --with-png-dir=/usr --with-jpeg-dir=/usr
 docker-php-ext-install $(grep -vE "^\s*#" /root/conf/php_extentions.conf  | tr "\n" " ")
+docker-php-ext-install sockets
 a2enmod rewrite
